e6266a9561f0b3f1769bcf2f246b95a40fc21ddd
Nick Ellsworth <nellsworth@spotify.com>
2021-08-02 12:54:55 -0400

    Updates

---
+++ precache-manifest.062c772ff3178c0ec9019ed59bc5e0a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "302503d6927cc98da52d74323313d850",
+    "url": "/frets/index.html"
+  },
+  {
+    "revision": "96afbedcb1cd18869129",
+    "url": "/frets/static/css/main.de5ef08a.chunk.css"
+  },
+  {
+    "revision": "ed57b44f312c595b61eb",
+    "url": "/frets/static/js/2.4d86cf95.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/frets/static/js/2.4d86cf95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96afbedcb1cd18869129",
+    "url": "/frets/static/js/main.68f00e30.chunk.js"
+  },
+  {
+    "revision": "4da96ad66d66ee08fdda",
+    "url": "/frets/static/js/runtime-main.d2af4442.js"
+  }
+]);
